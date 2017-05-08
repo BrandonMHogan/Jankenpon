0c20273920573d7841b3e0f2660491788ce26917
Brandon Hogan <brandon.hogan@speakfeel.ca>
2017-05-08 16:41:08 -0400

    first push

---
+++ jankenpon/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  jankenpon
+//
+//  Created by Brandon Hogan on 2017-05-08.
+//  Copyright © 2017 Brandon Hogan. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+final class Task: Object {
+    dynamic var text = ""
+    dynamic var completed = false
+}
