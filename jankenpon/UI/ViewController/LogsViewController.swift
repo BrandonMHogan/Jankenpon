73aca79f7a8435ca2cb9d0a17c44df777ba8720a
Brandon Hogan <brandon.hogan@speakfeel.ca>
2017-05-09 16:41:36 -0400

    messing around with navigation and segues

--- jankenpon/UI/ViewController/LogsViewController.swift
+++ jankenpon/UI/ViewController/LogsViewController.swift
@@ -1,30 +1,24 @@
 //
-//  ViewController.swift
+//  LogsViewController.swift
 //  jankenpon
 //
-//  Created by Brandon Hogan on 2017-05-08.
+//  Created by Brandon Hogan on 2017-05-09.
 //  Copyright © 2017 Brandon Hogan. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LogsViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("yolo")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func viewTapped(_ sender: UITapGestureRecognizer) {
-        self.performSegue(withIdentifier: "mySegue", sender: sender)
-    }
-
-
+    
+    
 }
-
